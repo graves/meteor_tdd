08d98715ab649a839b3f5576c12a02b21de8d5a0
Thomas Graves <thomas@ooo.pm>
2015-02-24 16:42:16 -0500

    Third passing test. Non admin cant create tutorials

--- tests/jasmine/client/integration/tutorialIntegrationSpec.js
+++ tests/jasmine/client/integration/tutorialIntegrationSpec.js
@@ -23,4 +23,26 @@ describe("Tutorial", function() {
       });
     });
   });
+
+  it("should not be created by non admins", function(done) {
+    // login to system and wait for callback
+    Meteor.loginWithPassword("normal@example.com", "normal420", function(err) {
+      // check if we correctly logged into the system
+      expect(err).toBeUndefined();
+
+      // create a new tuturial
+      var tut = new Tutorial();
+
+      // save the tuturial and use callback function to check for existence
+      var id = tut.save(function(error, result) {
+        expect(error.reason).toBe("Access denied");
+
+        Meteor.logout(function() {
+          done();
+        });
+      });
+    });
+  });
+
+
 });
