ebfc63cc6fdfb7529df3b846f4ee75c8902c9133
Thomas Graves <thomas@ooo.pm>
2015-02-26 15:47:26 -0500

    Finish and pass server unit tests

--- tests/jasmine/server/unit/tutorialDataModelSpec.js
+++ tests/jasmine/server/unit/tutorialDataModelSpec.js
@@ -38,12 +38,61 @@ describe("Tutorial", function() {
 
   it("should not save if name is not defined", function() {
     var tut = new Tutorial(null, "", 20);
+
     expect(function() { tut.save(); }).toThrow();
   });
 
   it("should not save if capacity is not defined", function() {
     var tut = new Tutorial(null, "Name", null);
+
     expect(function() { tut.save(); }).toThrow();
+  });
+
+  it("should allow students to register", function() {
+    var model = new Tutorial("1", "Name", 10, 5);
+    var studentId = "2";
+    spyOn(TutorialRegistrations, "insert").and.callFake(function(data, callback) {
+      callback(null);
+    });
+    spyOn(Tutorials, "update");
+    spyOn(Tutorials, "findOne").and.returnValue(null)
+    model.registerStudent(studentId);
+
+    expect(model.currentCapacity).toBe(6);
+    expect(TutorialRegistrations.insert).toHaveBeenCalled();
+    expect(TutorialRegistrations.insert.calls.mostRecent().args[0]).toEqual({ tutorialId: "1", studentId: "2" });
+    expect(Tutorials.update).toHaveBeenCalledWith({_id: "1"}, {$inc: {currentCapacity: 1}});
+  });
+
+  it("should not be possible to register while at maximum capacity", function() {
+    var model = new Tutorial(1, "Name", 5, 5);
+
+    expect(function() { model.registerStudent(1) }).toThrow("Capacity of tutorial has been reached!");
+  });
+
+  it("should not be possible to register if registration present", function() {
+    var tut = new Tutorial(1, "Name", 10, 9);
+    spyOn(TutorialRegistrations, "findOne").and.returnValue({});
+
+    expect(function() { tut.registerStudent(1) }).toThrow("Student already registered!");
+  });
+
+  it("should not be possible to de-register if registration not present", function() {
+    var tut = new Tutorial(1, "Name", 10, 9);
+    spyOn(Tutorials, "findOne").and.returnValue();
+
+    expect(function() { tut.removeRegistration(1) }).toThrow("Student not registered!");
+  });
+
+  it("should be possible to de-register if registered", function() {
+    var tut = new Tutorial(1, "Name", 10, 9);
+    spyOn(TutorialRegistrations, "findOne").and.returnValue({});
+    spyOn(TutorialRegistrations, "remove");
+    spyOn(Tutorials, "update");
+    tut.removeRegistration(2);
+
+    expect(TutorialRegistrations.remove).toHaveBeenCalledWith({tutorialId: 1, studentId: 2});
+    expect(Tutorials.update).toHaveBeenCalledWith({_id: 1}, {$inc: {currentCapacity: -1}});
   });
 
 });
